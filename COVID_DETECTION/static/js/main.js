3fe3d6f3860ab9acc64c81617aa5b27b8a056842
kdoranal <krupa.doranalu@wsu.edu>
2024-08-25 14:11:11 -0700

    Add COVID detection files

---
+++ COVID_DETECTION/static/js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    $('#result').hide();
+    $('#btn-predict').show();
+    $('.loader').hide();
+    // Upload Preview
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
+                $('#imagePreview').hide();
+                $('#imagePreview').fadeIn(650);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#imageUpload").change(function () {
+        $('#result').text('');
+        $('#result').hide();
+        readURL(this);
+    });
+
+    // Predict
+    $('#btn-predict').click(function () {
+        var form_data = new FormData($('#upload-file')[0]);
+        $('#btn-predict').hide();
+        $('.loader').show();
+        // Make prediction by calling api /predict
+        $.ajax({
+            type: 'POST',
+            url: '/predict',
+            data: form_data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            async: true,
+            success: function (data) {
+                $('#btn-predict').show();
+                $('.loader').hide();
+                $('#result').fadeIn(600);
+                $('#result').text(data);
+            },
+        });
+    });
+
+});
